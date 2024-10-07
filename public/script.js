8433e712b186a835f4fd900cf8f5c1e913112602
YuChenLin <53533541+YCLinYC@users.noreply.github.com>
2024-10-07 13:48:14 -0500

    Modified the time display

--- public/script.js
+++ public/script.js
@@ -1,8 +1,17 @@
 function updateTime() {
   const timeElement = document.getElementById('current-time');
   const now = new Date();
-  const timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit'});
-  timeElement.textContent = "Current Time: " + timeString;
+  const timeString = now.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+  const dateString = now.toLocaleDateString([], {
+    year: 'numeric', 
+    month: 'long',  // 'short' or 'numeric' for different formats
+    day: '2-digit'
+  });
+  timeElement.textContent = `Current Date: ${dateString}, Time: ${timeString}`;
 }
 
 // Update the time every second
